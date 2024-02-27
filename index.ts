1974889394831319320b9bf2b1177afc3bd0c028
jazqc <109171415+jazqc@users.noreply.github.com>
2024-02-27 14:49:18 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import dotenv from "dotenv";
 import { Server } from "./models/server";
 import { PrismaClient } from "@prisma/client";
-import createServer from "./models/server";
 
-const app = createServer();
 export const prisma = new PrismaClient();
 
 dotenv.config();
