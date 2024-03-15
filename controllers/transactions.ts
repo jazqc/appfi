5cef7e1442b245183a99ba01400cda4fdd6c300c
jazqc <109171415+jazqc@users.noreply.github.com>
2024-03-15 13:42:31 +0000

    updates

---
+++ controllers/transactions.ts
@@ -0,0 +1,13 @@
+// import { Request, Response } from "express";
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export const addTransaction =async (req: Request, res: Response) => {
+//     try {
+//         const userId: number = req.body.userConfirmed.user_id;
+//         const transactionData
+//     } catch (error) {
+        
+//     }
+// }
