2609bd4f33ab96eeab463e57b03e4092137048bc
jazqc <109171415+jazqc@users.noreply.github.com>
2024-03-18 06:48:17 -0300

    updates

--- dist/routes/transaction.js
+++ dist/routes/transaction.js
@@ -11,12 +11,12 @@ const transactions_1 = require("../controllers/transactions");
 const router = (0, express_1.Router)();
 router.post("/addTransaction", validateJWT_1.default, [
     (0, express_validator_1.check)("transaction_type_id", 'el tipo de transacción es obligatorio').not().isEmpty(),
-    (0, express_validator_1.check)("amout", 'el monto es obligatorio').not().isEmpty(),
+    (0, express_validator_1.check)("amount", 'el monto es obligatorio').not().isEmpty(),
     (0, express_validator_1.check)("name", 'Debe asignarle un título a la transacción').not().isEmpty(),
     (0, express_validator_1.check)("category_id", "Debe asignarle una categoría").not().isEmpty(),
     (0, express_validator_1.check)("date", "La fecha es obligatoria").not().isEmpty(),
     (0, express_validator_1.check)("category_id", "Debe asignarle una categoría").not().isEmpty(),
-    (0, express_validator_1.check)("user_payment_method", "Debe asignarle un método de pago").not().isEmpty(),
+    (0, express_validator_1.check)("user_payment_method_id", "Debe asignarle un método de pago").not().isEmpty(),
     recolectErrors_1.recolectErrors
 ], transactions_1.addTransaction);
 exports.default = router;
--- routes/transaction.ts
+++ routes/transaction.ts
@@ -10,12 +10,12 @@ router.post("/addTransaction",
 validarJWT,
 [   
     check ("transaction_type_id", 'el tipo de transacción es obligatorio').not().isEmpty(), 
-    check ("amout", 'el monto es obligatorio').not().isEmpty(),
+    check ("amount", 'el monto es obligatorio').not().isEmpty(),
     check ("name", 'Debe asignarle un título a la transacción').not().isEmpty(),
     check ("category_id", "Debe asignarle una categoría").not().isEmpty(),
     check ("date", "La fecha es obligatoria").not().isEmpty(),
     check ("category_id", "Debe asignarle una categoría").not().isEmpty(),
-    check ("user_payment_method", "Debe asignarle un método de pago").not().isEmpty(),
+    check ("user_payment_method_id", "Debe asignarle un método de pago").not().isEmpty(),
     recolectErrors
 ],
 addTransaction
